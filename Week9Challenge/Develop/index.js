028f15c5bf72dbc3d42586902d07443f75833950
Daniel <danieltang@Daniels-MacBook-Pro.local>
2022-07-25 13:04:24 -0500

    writing prompts for user

--- Week9Challenge/Develop/index.js
+++ Week9Challenge/Develop/index.js
@@ -1,7 +1,13 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What do you want the title of your repository to be?',
+    
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
